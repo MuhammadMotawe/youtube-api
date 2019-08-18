f5448aa00ae6b1af03320771e4ce3fc5f4d731c8
Muhammad Motawe <muhammad.a.motawe@gmail.com>
2019-08-18 07:55:27 +0200

    Well, just one commit is enough for this I guess

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { SearchBar, VideoDetails, VideoList } from './components';
+
+import youtube from './api/youtube';
+
+class App extends React.Component {
+    state = {
+        videos: [],
+        selectedVideo: null,
+    }
+
+    componentDidMount() {
+        this.handleSubmit('freeCodeCamp');
+    }
+
+    handleSubmit = async (searchTerm) => {
+        const res = await youtube.get('search', {params: { q: searchTerm, part: 'snippet', maxResults: 5, key: [API_KEY]}});
+
+        this.setState({
+            videos: res.data.items, selectedVideo: res.data.items[0]
+        });
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({
+            selectedVideo: video,
+        });
+    }
+
+    render() {
+        const { selectedVideo, videos } = this.state;
+        return (
+            <Grid container justify="center" spacing={10}>
+                <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={12}>
+                            <SearchBar onFormSubmit={this.handleSubmit} />
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetails video={selectedVideo} />
+                        </Grid>
+                        <Grid item xs={4}>
+                            <VideoList videos={videos} onVideoSelect={this.onVideoSelect} />
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/VideoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Grid } from '@material-ui/core';
+
+import VideoItem from './VideoItem';
+
+const VideoList = ({videos, onVideoSelect}) => {
+    const videoList = videos.map((video, id) => <VideoItem onVideoSelect={onVideoSelect} key={id} video={video} />);
+
+    return (
+        <Grid container spacing={10}>
+            {videoList}
+        </Grid>
+    );
+}
+
+export default VideoList;
